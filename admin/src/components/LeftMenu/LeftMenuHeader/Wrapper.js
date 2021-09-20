4ad5a46d061335c3e2a50dfab7fa477c1aa4fea4
Gustavo Cotta <gmcotta34@gmail.com>
2021-09-20 11:56:14 -0300

    change LeftMenuHeader component

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -1,29 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
 import Logo from '../../../assets/images/logo-won-white.svg';
 
 const Wrapper = styled.div`
-  background-color: ${(props) =>
-    props.theme.main.colors.leftMenu['background-header-link']};
-  height: ${props => props.theme.main.sizes.leftMenu.height};
-  .leftMenuHeaderLink {
-    &:hover {
-      text-decoration: none;
+  ${({ theme }) => css`
+    background-color: ${theme.main.colors.won.blue};
+    height: ${theme.main.sizes.leftMenu.height};
+    .leftMenuHeaderLink {
+      &:hover {
+        text-decoration: none;
+      }
     }
-  }
-  .projectName {
-    display: block;
-    width: 100%;
-    height: ${props => props.theme.main.sizes.leftMenu.height};
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-    color: $white;
-    background-image: url(${Logo});
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 12rem;
-  }
+    .projectName {
+      display: block;
+      width: 100%;
+      height: ${theme.main.sizes.leftMenu.height};
+      font-size: 2rem;
+      letter-spacing: 0.2rem;
+      color: $white;
+      background-image: url(${Logo});
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 12rem;
+    }
+
+  `}
 `;
 
 Wrapper.defaultProps = {
