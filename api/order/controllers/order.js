27a65f07542ab0fc92ed04324352a8d0b970d23e
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-05 07:57:50 -0300

    feat: get game info from request and calculate total price

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -6,8 +6,28 @@
  */
 
 module.exports = {
-  createPaymentIntent: () => {
-    return "Hello world"
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body
+    let games = []
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validGame = await strapi.services.game.findOne({ id: game.id })
+        if (validGame) games.push(validGame)
+      })
+    )
+    if(!games.length) {
+      ctx.response.status = 404
+      return {
+        error: 'No valid games found'
+      }
+    }
+    const total = games.reduce((acc, game) => {
+      return acc + game.price
+    }, 0)
+    if(total === 0) {
+      return { freeGames: true }
+    }
+    return {total_in_cents: total * 100 }
   }
 
 };
