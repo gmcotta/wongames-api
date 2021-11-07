42a4d807f526d493a1b064642b01b7cf0774012b
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-06 21:54:17 -0300

    feat: retrieve payment info and save it in database

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -37,11 +37,21 @@ module.exports = {
     const cartGameIds = await strapi.config.functions.cart.cartGameIds(cart)
     const games = await strapi.config.functions.cart.cartItems(cartGameIds)
     const total_in_cents = await strapi.config.functions.cart.total(games)
+    let paymentInfo
+    if (total_in_cents > 0) {
+      try {
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod)
+        console.log(paymentInfo)
+      } catch (error) {
+        ctx.response.status = 402
+        return { error: error.message }
+      }
+    }
     const entry = {
       total_in_cents,
       payment_intent_id: paymentIntentId,
-      card_brand: null,
-      card_last4: null,
+      card_brand: paymentInfo?.card?.brand,
+      card_last4: paymentInfo?.card?.last4,
       games,
       user: userInfo
     }
